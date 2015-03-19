e8ce3a17012fc06aba0d69796df2122415f87abd
Charles Hardes <hardesc@onid.oregonstate.edu>
2015-03-19 12:31:22 -0700

    Initial Commit

---
+++ Console_Cards/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Console_Cards
+//
+//  Created by Charles Hardes on 3/19/15.
+//  Copyright (c) 2015 Charles Hardes. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
