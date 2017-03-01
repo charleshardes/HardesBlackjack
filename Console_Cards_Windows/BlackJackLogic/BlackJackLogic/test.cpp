589077d316a240891cb212335646aa0977235010
Charles Hardes <charleshardes@gmail.com>
2017-03-01 09:26:17 -0800

    marshalling complete, about to attempt integration with dad's work

--- Console_Cards_Windows/BlackJackLogic/BlackJackLogic/test.cpp
+++ Console_Cards_Windows/BlackJackLogic/BlackJackLogic/test.cpp
@@ -5,6 +5,7 @@ extern "C" {
 #include <assert.h>
 #include <string.h>
 #include <direct.h>
+#include <time.h>
 #include <Windows.h>
 #include "cards_globals.h"
 #include "cards_structs.h"
@@ -437,6 +438,152 @@ extern "C" {
 
 	__declspec(dllexport) void DLLdisplayBet(int bet) { printf("Bet:    $%d", bet); }
 
+	__declspec(dllexport) void DLL_initTable(table *t, int NO_OF_PLAYERS, int NO_OF_COMPS) {
+
+		int i, j = 0;
+		char comp[10], playerNo[2], playerNoPtr[2];
+		assert(t);
+
+		t->dealer = createPlayer("Dealer", 0, 1, 1);
+		for (i = 0; i < NO_OF_PLAYERS - NO_OF_COMPS; i++) {
+			t->players[i] = createPlayer(playerArr[i], (i + 1), 0, 0);
+			j = i + 1;
+		}
+		t->NO_OF_PLAYERS = NO_OF_PLAYERS;
+
+		for (i = 0; i< NO_OF_COMPS; i++) {
+			playerNo[0] = (char)(((int)'0') + i + 1);/*convert int to char*/
+			playerNo[1] = '\0';/*make string from char by adding endline character*/
+			strcpy_s(playerNoPtr, sizeof playerNoPtr, playerNo);
+			strcpy_s(comp, sizeof comp, "Computer");
+			strcat_s(comp, sizeof comp, playerNoPtr);
+			t->players[j] = createPlayer(comp, (j + 1), 0, 1);
+			j++;
+			//fgfdfdfdg
+		}
+		t->NO_OF_COMPS = NO_OF_COMPS;
+
+		/*create a deck to be pointed to by discardPile*/
+		t->discardPile = (deck *)malloc(sizeof(deck));
+
+		/*create array of pointers to struct card for discard pile*/
+		for (i = 0; i < NO_OF_CARDS; i++) {
+			t->discardPile->cards[i] = NULL;
+		}
+		t->discardPile->cards_left = 0;
+		t->discardPile->top = NULL;
+
+		t->currPlayer = 0;
+		t->spacing = 0;
+		t->margin = (int)(10 - (t->NO_OF_PLAYERS * 2.5));
+		t->handsAreDealt = 0;
+		t->hasSplits = 0;
+	}
+
+	__declspec(dllexport) table *DLLSetTable(int NO_OF_PLAYERS, int NO_OF_COMPS, char *names[]) {
+
+		int no_Total_Players, i;
+
+		for (i = 0; i < NO_OF_PLAYERS; i++) {
+			strcpy_s(playerArr[i], sizeof playerArr[i], names[i]);
+		}
+
+		no_Total_Players = NO_OF_PLAYERS + NO_OF_COMPS;
+
+		return createTable(no_Total_Players, NO_OF_COMPS);
+	}
+
+	__declspec(dllexport) deck *DLLcreateDeck() {
+
+		struct deck *newDeck = (deck *)malloc(sizeof(deck));
+		_initDeck(newDeck);
+		return newDeck;
+	}
+
+	__declspec(dllexport) deck *DLLshuffle(deck *d) {
+
+		time_t t;
+		int i, n, cardsLeft;
+		deck *shuffled;
+		assert(d && !isEmpty(d));
+
+		/*create, initialize temporary deck to all cards NULL*/
+		cardsLeft = d->cards_left;
+		shuffled = (deck *)malloc(sizeof(deck));
+		clearDeck(shuffled);
+
+		/* Intializes random number generator */
+		srand((unsigned)time(&t));
+
+		/* puts each of the cards from d into a random slot in shuffled */
+		for (i = 0; i < cardsLeft; i++) {
+			n = rand() % cardsLeft;
+			while (shuffled->cards[n] != NULL) {
+				n++;/*find empty slot*/
+				if (n == cardsLeft) { n = 0; }/*loop back to beginning of deck*/
+			}
+			shuffled->cards[n] = d->cards[i];/*puts card in empty slot*/
+		}
+
+		/*assign shuffled cards back to original deck with rest of cards NULL*/
+		for (i = 0; i < NO_OF_CARDS; i++) {
+			d->cards[i] = shuffled->cards[i];
+		}
+
+		d->top = d->cards[cardsLeft - 1];/*sets last card to top card*/
+
+		free(shuffled);
+		return d;
+
+	}
+
+	__declspec(dllexport) void DLLcleanUp(table *t, deck *d) {
+		int i, h, players;
+		assert(t);
+		assert(d);
+
+		players = t->NO_OF_PLAYERS;
+		_deletePlayer(t->dealer);
+
+		for (i = 0; i < players; i++) {
+			_deletePlayer(t->players[i]);
+		}
+		free(t->discardPile);
+		//free(t);
+			
+
+		_deleteCardSuits(suitArr);
+		_deleteCardValues(valueArr);
+		if (HI_LO_ACES == 1) {
+			for (h = 0; h < NO_OF_SUITS; h++) {
+				_deleteCardValue(hi_loAceArr[h]);
+			}
+		}
+#if (NO_OF_EXTRA_CARDS > 0)
+		_deleteExtraCards(extraCardsArr);
+#endif
+
+		for (i = 0; i < NO_OF_CARDS; i++) {
+			_deleteCard(d->cards[i]);
+		}
+		//free(d);
+	}
+
+	__declspec(dllexport) void DLLsetAllHands(table *t) {
+
+		int i;
+
+		/*create hand structs for each player*/
+		for (i = 0; i < (t->NO_OF_PLAYERS); i++) {
+			setNewHand(t->players[i]->playerHand);
+		}
+
+		/*create hand struct for dealer*/
+		setNewHand(t->dealer->playerHand);
+
+		/*reset handsAreDealt attribute back to false*/
+		t->handsAreDealt = 0;
+	}
 
 #if ( 1 )
 	//
@@ -449,11 +596,11 @@ extern "C" {
 	//  This is the main driver for HardesBlackJack
 	//
 
-	__declspec(dllexport) void runBlackjack() {
+	__declspec(dllexport) void runBlackjack(table *BJTable) {
 
 		int cont;/* boolean 1 or 0 (True/False) variable used only for determining whether
 				 user has chosen to continue (deal again) or quit the program*/
-		table *BJTable;/* pointer to main BlackJack Table object created and used in the program */
+		//table *BJTable;/* pointer to main BlackJack Table object created and used in the program */
 		deck *BJdeck;/*   pointer to main BlackJack Deck object created and used in the program */
 		//FILE *fp;
 
@@ -462,7 +609,7 @@ extern "C" {
 
 		/*get the # of players, players' names, # of comps, create the table,
 		return a pointer to it*/
-		BJTable = setTable();
+		//BJTable = setTable();
 
 		/*create the blackjack deck, return a pointer to it*/
 		BJdeck = createDeck();
--- Console_Cards_Windows/HelloDLL/HelloDLL/Program.cs
+++ Console_Cards_Windows/HelloDLL/HelloDLL/Program.cs
@@ -7,13 +7,21 @@ using System.Runtime.InteropServices;
 using System.IO;
 
 
+
 namespace HelloDLL {
+
+    
 
     //public delegate int runIt();
 
-    static class Constants {
+    static class Constants
+    {
         public const string dllPath = @"C:\Users\Charles\Documents\GitHub\HardesBlackJack\Console_Cards_Windows\Debug\BlackJackLogic.dll";
+        //public const string dllPath = @"C:\Users\Charles Hardes\Source\Repos\HardesBlackJack\Console_Cards_Windows\Debug\BlackJackLogic.dll";
         public const string newInPath = @"C:\Users\Charles\Documents\GitHub\HardesBlackJack\Console_Cards_Windows\HelloDLL\HelloDLL\bin\Debug\newIn.txt";
+        public const int NO_OF_CARDS = 52;
+        public const int NO_OF_SUITS = 4;
+        public const int NO_OF_CARD_VALUES = 13;
     }
 
 
@@ -62,48 +70,117 @@ namespace HelloDLL {
     //------------------------------------MARSHALED BJ STRUCTS-------------------------------------------
 
 
-
-    [StructLayout(LayoutKind.Sequential, CharSet=CharSet.Ansi)]
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi)]
     public struct cardValue {
 
-        [MarshalAs(UnmanagedType.LPStr, SizeConst = 10)]
-        public string name;
-
-        [MarshalAs(UnmanagedType.U1)]
-        public char abbr;
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 10)]
+        public char[] name;
+        public byte abbr;
         public int weight;
     };
 
-    [StructLayout(LayoutKind.Sequential)]
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi)]
     public struct cardSuit {
-        public string name;
-        public char abbr;
-        public bool trump;
+
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 10)]
+        public char[] name;
+        public byte abbr;
+        public int trump;
         public int weight;
     };
 
-    [StructLayout(LayoutKind.Sequential, CharSet=CharSet.Ansi)]
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi)]
     public struct card {
+
         public IntPtr suit;
         public IntPtr value;
-        public string name;
-        public string abbr;
-        public bool shown;
-        public bool wildcard;
+
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 25)]
+        public char[] name;
+
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 3)]
+        public char[] abbr;
+        public int shown;
+        public int wildcard;
     };
- 
-/*
-    [StructLayout(LayoutKind.Sequential)]
-    public struct deck
-    {
-        [MarshalAs(UnmanagedType.Struct)]
-        card* cards[NO_OF_CARDS];
-        cardSuit* suits[NO_OF_SUITS];
-        cardValue* values[NO_OF_CARD_VALUES];
+
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi)]
+    public struct deck {
+
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = Constants.NO_OF_CARDS)]
+        public IntPtr[] cards;//initialize array of cards ptrs
+
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = Constants.NO_OF_SUITS)]
+        public IntPtr[] suits;
+
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = Constants.NO_OF_CARD_VALUES)]
+        public IntPtr[] values;
         public IntPtr top;
+
         public int cards_left;
     };
- */       
+
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi)]
+    public struct hand {
+
+        public int starting;
+
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 22)]
+        public IntPtr[] cards;
+
+        public int score;
+        public int bust;
+        public int canSplit;
+        public int hasSplit;
+        public IntPtr splitHand;
+        public int hasBlackjack;
+        public int cardCount;
+        public int hiAces;
+        public int win;
+        public int lose;
+        public int push;
+        public int hasInsurance;
+        public int insuranceAmt;
+        public int doubledDown;
+        public int hasEnded;
+        public int bet;
+        public int handIndex;
+    };
+
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi)]
+    public struct player {
+
+        public int dealer;
+        public int computer;
+
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 20)]
+        public char[] name;
+
+        public int pos;
+        public IntPtr playerHand;
+        public int chips;
+        public int handCount;
+    };
+
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi)]
+    public struct table {
+
+        public IntPtr dealer;
+
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
+        public IntPtr[] players;
+
+        public int NO_OF_PLAYERS;
+        public int NO_OF_COMPS;
+        public IntPtr discardPile;
+        public int currPlayer;
+        public int spacing;
+        public int margin;
+        public int handsAreDealt;
+        public int hasSplits;
+    };
+
+
     static class Program {
         /// <summary>
         /// The main entry point for the application.
@@ -134,7 +211,7 @@ namespace HelloDLL {
         public static extern void DLLhideCard(ref card c);
 
         [DllImport(Constants.dllPath)]
-        public static extern int runBlackjack();
+        public static extern int runBlackjack(ref table t);
 
 
 
@@ -232,6 +309,8 @@ namespace HelloDLL {
 
         //-----------------------------------------structArray Tests-------------------------
 
+        //new shit for gitss
+
         [DllImport(Constants.dllPath)]
         public static extern void testTheStructArr1();
 
@@ -249,38 +328,134 @@ namespace HelloDLL {
         [DllImport(Constants.dllPath, CallingConvention =CallingConvention.Cdecl)]
         public static extern void DLLChangeCardVal(ref cardValue cv);
 
+
+        //------------------------------------------BJ DEFS----------------------------------
+
+        [DllImport(Constants.dllPath, CallingConvention = CallingConvention.Cdecl)]
+        public static extern IntPtr DLLcreateTable(int NO_OF_PLAYERS, int NO_OF_COMPS);
+
+        [DllImport(Constants.dllPath, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void DLL_initTable(ref table t, int NO_OF_PLAYERS, int NO_OF_COMPS);
+
+        [DllImport(Constants.dllPath, CallingConvention = CallingConvention.Cdecl)]
+        public static extern IntPtr DLLSetTable(int NO_OF_PLAYERS, int NO_OF_COMPS, string[] names);
+
+        [DllImport(Constants.dllPath, CallingConvention = CallingConvention.Cdecl)]
+        public static extern IntPtr DLLcreateDeck();
+
+        [DllImport(Constants.dllPath, CallingConvention = CallingConvention.Cdecl)]
+        public static extern IntPtr DLLshuffle(ref deck d);
+
+        [DllImport(Constants.dllPath, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void DLLcleanUp(ref table t, ref deck d);
+
+        [DllImport(Constants.dllPath, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void DLLsetAllHands(ref table t);
+
+
         [STAThread]
         static void Main() {
+
+            int NO_OF_PLAYERS, NO_OF_COMPS;
+            table BJtable;
+            deck BJdeck, BJdeck2;
+            IntPtr d2, d3;
+            bool cont = true;
             
 
-            IntPtr notPointer = (IntPtr) 29;
+            /***********************GAME START*************************
+             * initialize GUI stuff, do variable declarations, etc here
+             * 
+             * 
+             * ********************************************************
+            
 
-            card MyCard;
-            MyCard.suit = notPointer;
-            MyCard.value = notPointer;
-            MyCard.name = "Two of Spades";
-            MyCard.abbr = "2s";
-            MyCard.wildcard = false;
-            MyCard.shown = true;
 
-            //DLLhideCard(ref MyCard);
+            /*********************** GET PLAYER INPUT FROM FORM*********
+             * Present first game form here, get number of human players,
+             * number of computer players, names of human players
 
-            if (MyCard.shown)
-            {
-                Console.Write("is shown");
+            ***********************************************************/
+            NO_OF_PLAYERS = 4;
+            NO_OF_COMPS = 0;
+            string[] stringArr = { "Charles", "Theresa", "Charlie", "Nathanael" };
+
+
+            player[] p = new player[NO_OF_PLAYERS];
+
+            IntPtr t = DLLSetTable(NO_OF_PLAYERS, NO_OF_COMPS, stringArr);
+
+            BJtable = (table)Marshal.PtrToStructure(t, typeof(table));
+
+            for (int i = 0; i < NO_OF_PLAYERS + NO_OF_COMPS; i++) {
+               p[i] = (player)Marshal.PtrToStructure(BJtable.players[i], typeof(player));
             }
-            else
-            {
-                Console.Write("hidden");
-            }
 
-            cardValue myCV;
+            IntPtr d = DLLcreateDeck();
 
-            myCV.abbr = '5';
-            myCV.name = "Five";
-            myCV.weight = 5;
+            BJdeck = (deck)Marshal.PtrToStructure(d, typeof(deck));
 
-            DLLChangeCardVal(ref myCV);
+            d2 = DLLshuffle(ref BJdeck);
+
+            BJdeck2 = (deck)Marshal.PtrToStructure(d2, typeof(deck));
+
+
+            //************************GAME LOOP*************************
+            do {
+
+                DLLsetAllHands(ref BJtable);
+
+
+                cont = false;
+            } while (cont == true);
+
+
+
+
+
+
+
+
+
+
+            DLLcleanUp(ref BJtable, ref BJdeck);
+
+            return;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
             //-------------------TESTING testStruct----------------------------------------//
 
@@ -426,7 +601,11 @@ namespace HelloDLL {
                 Marshal.FreeHGlobal(sa.rts[i]);
             }
 
-            runBlackjack();
+
+
+
+
+            //runBlackjack(ref t);
 
             //Marshal.FreeHGlobal(SA);
 
